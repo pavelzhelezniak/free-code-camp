41c9436e115f7c4f1d659b930e2008eb7d73ecbc
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-09 13:39:49 +0300

    solved 5,6,7,8 tasks

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -18,3 +18,39 @@ a = 7;
 
 var b;
 b = a;
+
+// 5 task. Define a variable a with var and initialize it to a value of 9.
+
+var a = 9;
+
+// 6 task. Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+
+var a;
+a = 5;
+var b;
+b = 10;
+var c;
+c = "I am a"
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// 7 task. Modify the existing declarations and assignments so their names use camelCase. Do not create any new variables.
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// 8 task. Change the 0 so that sum will equal 20. (var sum = 10 + 0)
+
+var sum = 10 + 10;
+
+
+
+
+
