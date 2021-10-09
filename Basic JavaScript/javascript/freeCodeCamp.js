5ed20d89db7ca7481e5be21c7c39e884eb1b0e2c
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-09 13:14:17 +0300

    added new files index.html, freeCodeCamp.css, freeCodeCamp.js

---
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -0,0 +1,15 @@
+// 1 task. Try creating one of each type of comment.
+
+// First task
+/*
+First task
+ */
+
+// 2 task. Use the var keyword to create a variable called myName.
+
+var myName;
+
+// 3 task. Assign the value 7 to variable a.
+
+var a;
+a = 7;
