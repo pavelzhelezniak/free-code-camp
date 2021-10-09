db24bad19df2d0f000c134539c8c01b60a199417
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-09 14:21:16 +0300

    solved 18-26 tasks

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -88,3 +88,100 @@ var quotient = 4.4 / 2.0
 
 var remainder = 11 % 3;
 
+// 18 task. Convert the assignments for a, b, and c to use the += operator.
+var a = 3;
+var b = 17;
+var c = 12;
+
+/* a = a + 12;
+b = 9 + b;
+c = c + 7; */
+
+a += 12;
+b += 9;
+c += 7;
+
+// 19 task. Convert the assignments for a, b, and c to use the -= operator.
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+/* a = a - 6;
+b = b - 15;
+c = c - 1; */
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+// 20 task. Convert the assignments for a, b, and c to use the *= operator.
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+/* a = a * 5;
+b = 3 * b;
+c = c * 10; */
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+// 21 task. Convert the assignments for a, b, and c to use the /= operator.
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+/* a = a / 12;
+b = b / 4;
+c = c / 11; */
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+// 22 task. Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+var myFirstName = 'Pavel';
+var myLastName = 'Zheleznyak';
+
+// 23 task. Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+//I am a "double quoted" string inside "double quotes".
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+// 24 task. Change the provided string to a string with single quotes at the beginning and end and no escape characters.
+// var myStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// 25 task. Assign the following three lines of text into the single variable myStr using escape sequences.
+
+/* FirstLine
+	\SecondLine
+ThirdLine */
+
+var myStr = 'FirstLine\n\t\\SecondLine\nThirdLine';
+
+// 26 task. Build myStr from the strings 'This is the start.' and 'This is the end.' using the + operator. Be sure to include a space between the two strings.
+
+var myStr = 'This is the start. ' + 'This is the end.';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
