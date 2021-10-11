2c7aa1b8f87f4aa8e3eb3c8ef1dd62f989346e1d
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-11 19:45:34 +0300

    solved 42 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -259,5 +259,21 @@ myArray[0] = 45;
 var myArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [[10, 11, 12], 13, 14]];
 var myData = myArray[2][1];
 
+//41 task. Push ["dog", 3] onto the end of the myArray variable.
+
+var myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
