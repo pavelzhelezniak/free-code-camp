b412d76b8730a03b8de69b28fdabf4134bfed6de
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-11 19:42:35 +0300

    solved 41 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -254,14 +254,10 @@ var myData = myArray[0];
 var myArray = [18, 64, 99];
 myArray[0] = 45;
 
+//41 task. Using bracket notation select an element from myArray such that myData is equal to 8.
 
-
-
-
-
-
-
-
+var myArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [[10, 11, 12], 13, 14]];
+var myData = myArray[2][1];
 
 
 
