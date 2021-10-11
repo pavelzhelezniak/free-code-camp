42d9dfdbc20e628e6b72c95b732e71482f326a4a
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-11 23:51:36 +0300

    solved 52 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -339,3 +339,13 @@ function myOutfit() {
 }
 
 myOutfit();
+
+//51 task. Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+function timesFive(num) {
+	return num * 5;
+}
+
+
+
+
