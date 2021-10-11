e96cdd3a46e4dfa69b19ba0bc4e9b3f7f9b5fe1d
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-11 23:56:19 +0300

    solved 54 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -360,3 +360,14 @@ function addFive() {
 
 addThree();
 addFive();
+
+//54 task. Call the processArg function with an argument of 7 and assign its return value to the variable processed.
+
+var processed = 0;
+
+function processArg(num) {
+	return (num + 3) / 5;
+}
+
+processed = processArg(7);
+
