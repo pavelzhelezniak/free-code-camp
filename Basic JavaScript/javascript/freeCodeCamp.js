184f18377d2c4222302e1870a1b541571e37e05d
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-11 23:41:57 +0300

    solved 49 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -299,7 +299,21 @@ function functionWithArgs(firstParam, secondParam) {
 }
 functionWithArgs(3, 4);
 
+//49 Using var, declare a global variable named myGlobal outside of any function. Initialize it with a value of 10. Inside function fun1, assign 5 to oopsGlobal without using the var keyword.
 
+var myGlobal = 10;
 
+function fun1() {
+	oopsGlobal = 5;
+}
 
-
+function fun2() {
+	var output = "";
+	if (typeof myGlobal != "undefined") {
+		output += "myGlobal: " + myGlobal;
+	}
+	if (typeof oopsGlobal != "undefined") {
+		output += " oopsGlobal: " + oopsGlobal;
+	}
+	console.log(output);
+}
