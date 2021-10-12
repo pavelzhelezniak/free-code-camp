28e580c6c77cf7e6dd0c224f2c69cdebac4078b8
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-12 17:48:48 +0300

    solved 69 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -543,8 +543,22 @@ function testLogicalOr(val) {
 
 testLogicalOr(15);
 
+//69 task. Combine the if statements into a single if/else statement.
 
+function testElse(val) {
+	var result = "";
+	// Only change code below this line
 
+	if (val > 5) {
+		result = "Bigger than 5";
+	} else {
+		result = "5 or Smaller";
+	}
 
+	// Only change code above this line
+	return result;
+}
+
+testElse(4);
 
 
