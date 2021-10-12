d5d4dfc4568da86df687686807647173d606d53f
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-12 17:05:41 +0300

    solved 58 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -398,9 +398,16 @@ function trueOrFalse(wasThatTrue) {
 	return 'No, that was false';
 }
 
+//58 task. Add the equality operator to the indicated line so that the function will return the string Equal when val is equivalent to 12.
 
+function testEqual(val) {
+	if (val == 12) { // Change this line
+		return "Equal";
+	}
+	return "Not Equal";
+}
 
-
+testEqual(10);
 
 
 
