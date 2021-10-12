8588c0fd93b7a2f69649caaa09d683ab7ae5cbe3
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-12 17:09:03 +0300

    solved 60 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -420,6 +420,27 @@ function testStrict(val) {
 
 testStrict(10);
 
+//60 task. The compareEquality function in the editor compares two values using the equality operator. Modify the function so that it returns the string Equal only when the values are strictly equal.
+
+function compareEquality(a, b) {
+	if (a == b) { // Change this line
+		return "Equal";
+	}
+	return "Not Equal";
+}
+
+compareEquality(10, "10");
+
+
+
+
+
+
+
+
+
+
+
 
 
 
