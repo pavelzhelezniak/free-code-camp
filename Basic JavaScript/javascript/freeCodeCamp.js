b4831ae714f270544472cf5bf0f78b2dbd5fb62a
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-12 17:07:06 +0300

    solved 59 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -409,9 +409,16 @@ function testEqual(val) {
 
 testEqual(10);
 
+//59 task. Use the strict equality operator in the if statement so the function will return the string Equal when val is strictly equal to 7
 
+function testStrict(val) {
+	if (val === 7) { // Change this line
+		return "Equal";
+	}
+	return "Not Equal";
+}
 
-
+testStrict(10);
 
 
 
