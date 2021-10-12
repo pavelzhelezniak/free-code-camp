cf03e6fc54e639f83b7926e8e33452757d276a1f
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-12 17:32:13 +0300

    solved 63 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -453,8 +453,20 @@ function testStrictNotEqual(val) {
 
 testStrictNotEqual(10);
 
+//63 task. Add the greater than operator to the indicated lines so that the return statements make sense.
 
+function testGreaterThan(val) {
+	if (val > 100) {  // Change this line
+		return "Over 100";
+	}
 
+	if (val > 10) {  // Change this line
+		return "Over 10";
+	}
 
+	return "10 or Under";
+}
+
+testGreaterThan(10);
 
 
