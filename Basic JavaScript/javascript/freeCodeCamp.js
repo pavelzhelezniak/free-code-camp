cb9a2a0b083949a9d0baafb72b7a997e8e3df197
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-12 17:41:29 +0300

    solved 67 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -517,9 +517,18 @@ function testLessOrEqual(val) {
 
 testLessOrEqual(10);
 
+//67 task. Replace the two if statements with one statement, using the && operator, which will return the string Yes if val is less than or equal to 50 and greater than or equal to 25. Otherwise, will return the string No.
 
+function testLogicalAnd(val) {
 
+	if (val <= 50 && val >= 25) {
+		return "Yes";
+	}
 
+	return "No";
+}
+
+testLogicalAnd(10);
 
 
 
