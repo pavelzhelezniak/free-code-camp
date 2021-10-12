9e01dc8050dfd40fd53e160fa162b62129a70558
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-12 17:45:22 +0300

    solved 68 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -530,9 +530,18 @@ function testLogicalAnd(val) {
 
 testLogicalAnd(10);
 
+//68 task. Combine the two if statements into one statement which returns the string Outside if val is not between 10 and 20, inclusive. Otherwise, return the string Inside.
 
+function testLogicalOr(val) {
 
+	if (val < 10 || val > 20) {
+		return "Outside";
+	}
 
+	return "Inside";
+}
+
+testLogicalOr(15);
 
 
 
