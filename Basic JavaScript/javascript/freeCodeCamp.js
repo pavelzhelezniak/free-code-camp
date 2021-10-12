0f65813c61b1c276b52a44928db1a8b56dc9c5cd
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-12 17:11:23 +0300

    solved 61 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -431,9 +431,16 @@ function compareEquality(a, b) {
 
 compareEquality(10, "10");
 
+//61 task. Add the inequality operator != in the if statement so that the function will return the string Not Equal when val is not equivalent to 99
 
+function testNotEqual(val) {
+	if (val != 99) { // Change this line
+		return "Not Equal";
+	}
+	return "Equal";
+}
 
-
+testNotEqual(10);
 
 
 
