1b4054a418cd89e047052e7c3102f9568b6c9327
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-13 18:05:26 +0300

    solved 92 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -960,3 +960,47 @@ var myStorage = {
 };
 
 var gloveBoxContents = myStorage.car.inside["glove box"];
+
+//92 task. Accessing Nested Arrays
+
+var myPlants = [
+	{
+		type: "flowers",
+		list: [
+			"rose",
+			"tulip",
+			"dandelion"
+		]
+	},
+	{
+		type: "trees",
+		list: [
+			"fir",
+			"pine",
+			"birch"
+		]
+	}
+];
+
+var secondTree = myPlants[1].list[1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
