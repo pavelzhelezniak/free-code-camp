2197dbed472cd6594095a6efd3141de65b808134
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-13 14:42:28 +0300

    solved 78 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -750,5 +750,12 @@ function chainToSwitch(val) {
 
 chainToSwitch(7);
 
+//78 task. Returning Boolean Values from Functions
 
+function isLess(a, b) {
+	// Only change code below this line
+	return a < b;
+	// Only change code above this line
+}
 
+isLess(10, 15);
