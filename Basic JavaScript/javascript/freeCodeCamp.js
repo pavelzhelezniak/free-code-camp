83a43d17a92166261f1abaa21c6052ee32dc87d9
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-13 17:57:56 +0300

    solved 90 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -918,9 +918,32 @@ function checkObj(obj, checkProp) {
 
 }
 
+//90 task. Manipulating Complex Objects
 
-
-
+var myMusic = [
+	{
+		"artist": "Billy Joel",
+		"title": "Piano Man",
+		"release_year": 1973,
+		"formats": [
+			"CD",
+			"8T",
+			"LP"
+		],
+		"gold": true
+	},
+	{
+		"artist": "Michael Jackson",
+		"title": "Thriller",
+		"release_year": 1982,
+		"formats": [
+			"CD",
+			"8T",
+			"LP"
+		],
+		"gold": false
+	}
+];
 
 
 
