421c6f932e8b2bde05d8a9ee8ebcf7de6760f0c2
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-13 14:32:31 +0300

    solved 76 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -668,7 +668,7 @@ function caseInSwitch(val) {
 
 caseInSwitch(1);
 
-//75 Adding a Default Option in Switch Statements
+//75 task. Adding a Default Option in Switch Statements
 
 function switchOfStuff(val) {
 	var answer = "";
@@ -693,9 +693,33 @@ function switchOfStuff(val) {
 
 switchOfStuff(1);
 
+//76 task. Multiple Identical Options in Switch Statements
 
+function sequentialSizes(val) {
+	var answer = "";
+	// Only change code below this line
+	switch (val) {
+		case 1:
+		case 2:
+		case 3:
+			answer = "Low";
+			break;
+		case 4:
+		case 5:
+		case 6:
+			answer = "Mid";
+			break;
+		case 7:
+		case 8:
+		case 9:
+			answer = "High";
+			break;
+	}
+	// Only change code above this line
+	return answer;
+}
 
-
+sequentialSizes(1);
 
 
 
