b665152f920721d5f238a43dad71d8da4a4306a9
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-13 14:21:38 +0300

    solved 74 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -642,3 +642,28 @@ function golfScore(par, strokes) {
 }
 
 golfScore(5, 4);
+
+//74 task. Selecting from Many Options with Switch Statements
+
+function caseInSwitch(val) {
+	var answer = "";
+	switch (val) {
+		case 1:
+			answer = "alpha";
+			break;
+		case 2:
+			answer = "beta";
+			break;
+		case 3:
+			answer = "gamma";
+			break;
+		case 4:
+			answer = "delta";
+			break;
+	}
+	// Only change code below this line
+	// Only change code above this line
+	return answer;
+}
+
+caseInSwitch(1);
