b851dee1c0765d3ed5d7a3b4b5a4ef02b75cfad7
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-14 18:20:46 +0300

    solved 8 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -77,3 +77,21 @@ const magic = () => new Date();
 const myConcat = (arr1, arr2) => arr1.concat(arr2);
 
 console.log(myConcat([1, 2], [3, 4, 5]));
+
+// 8 task. Set Default Parameters for Your Functions
+
+const increment = (number, value = 1) => number + value;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
