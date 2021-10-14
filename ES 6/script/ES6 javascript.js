7a70739d815628320889a47ac70f3a0ca173964a
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-14 18:08:28 +0300

    solved 4 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -37,9 +37,18 @@ function printManyTimes(str) {
 
 printManyTimes("freeCodeCamp");
 
+// 4 task. Mutate an Array Declared with const
 
+const s = [5, 7, 2];
+function editInPlace() {
 
+	s[0] = 2;
+	s[1] = 5;
+	s[2] = 7;
 
+}
+
+editInPlace();
 
 
 
