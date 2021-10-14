83b061b09783251f1f247a70ebcf15cfd0394e1e
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-14 15:10:06 +0300

    solved 105 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -1171,9 +1171,13 @@ function randomWholeNum() {
 
 }
 
+// 105 task. Generate Random Whole Numbers within a Range
 
+function randomRange(myMin, myMax) {
 
+	return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
 
+}
 
 
 
