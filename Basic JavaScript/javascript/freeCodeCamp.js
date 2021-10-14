6bd820bd7b0cb1fab530538f530104f8d90e1edd
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-14 14:56:30 +0300

    solved 102 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -1103,8 +1103,57 @@ function sum(arr, n) {
 
 }
 
+//102 task. Profile Lookup
+
+var contacts = [
+	{
+		"firstName": "Akira",
+		"lastName": "Laine",
+		"number": "0543236543",
+		"likes": ["Pizza", "Coding", "Brownie Points"]
+	},
+	{
+		"firstName": "Harry",
+		"lastName": "Potter",
+		"number": "0994372684",
+		"likes": ["Hogwarts", "Magic", "Hagrid"]
+	},
+	{
+		"firstName": "Sherlock",
+		"lastName": "Holmes",
+		"number": "0487345643",
+		"likes": ["Intriguing Cases", "Violin"]
+	},
+	{
+		"firstName": "Kristian",
+		"lastName": "Vos",
+		"number": "unknown",
+		"likes": ["JavaScript", "Gaming", "Foxes"]
+	}
+];
 
 
+function lookUpProfile(name, prop) {
+
+	for (let i = 0; i < contacts.length; i++) {
+		if (contacts[i]['firstName'] == name && contacts[i].hasOwnProperty(prop)) {
+			return contacts[i][prop];
+		} else if (contacts[i]['firstName'] == name && !contacts[i].hasOwnProperty(prop)) {
+			return 'No such property';
+		}
+	}
+
+	for (let i = 0; i < contacts.length; i++) {
+
+		if (contacts[i]['firstName'] !== name) {
+			return 'No such contact';
+		}
+	}
+
+
+}
+
+lookUpProfile("Akira", "likes");
 
 
 
