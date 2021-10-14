8344ddfe2cd617d45b122b7f633fc73d0b81aa92
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-14 18:31:56 +0300

    solved 11 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -99,8 +99,14 @@ arr2 = [...arr1];
 
 console.log(arr2);
 
+// 11 task. Use Destructuring Assignment to Extract Values from Objects
 
+const HIGH_TEMPERATURES = {
+	yesterday: 75,
+	today: 77,
+	tomorrow: 80
+};
 
-
+const { today, tomorrow } = HIGH_TEMPERATURES;
 
 
