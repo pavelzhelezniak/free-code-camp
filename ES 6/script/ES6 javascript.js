a13517da36926f1efa1f6d1504813989a33796a3
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-14 18:05:23 +0300

    solved 3 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -23,3 +23,31 @@ function checkScope() {
 	console.log('Function scope i is: ', i);
 	return i;
 }
+
+// 3 task. Declare a Read-Only Variable with the const Keyword
+
+function printManyTimes(str) {
+
+	const SENTENCE = str + " is cool!";
+	for (let i = 0; i < str.length; i += 2) {
+		console.log(SENTENCE);
+	}
+
+}
+
+printManyTimes("freeCodeCamp");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
