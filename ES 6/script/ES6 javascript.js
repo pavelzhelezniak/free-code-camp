fbe655760a0e77b3d2924274f2d46f6edc5bd68a
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-14 18:18:39 +0300

    solved 7 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -72,5 +72,8 @@ const PI = freezeObj();
 
 const magic = () => new Date();
 
+// 7 task. Write Arrow Functions with Parameters
 
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
 
+console.log(myConcat([1, 2], [3, 4, 5]));
