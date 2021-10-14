2730d4807178a2c6245eff8789c1af673d42ea78
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-14 18:28:41 +0300

    solved 10 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -90,9 +90,14 @@ const sum = (...args) => {
 
 }
 
+// 10 task. Use the Spread Operator to Evaluate Arrays In-Place
 
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
 
+arr2 = [...arr1];
 
+console.log(arr2);
 
 
 
