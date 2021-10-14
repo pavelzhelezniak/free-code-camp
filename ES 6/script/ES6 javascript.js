80266b38be761f8851084296f2301fd1c2d433af
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-14 17:46:42 +0300

    solved 2 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -10,3 +10,16 @@ function catTalk() {
 
 }
 catTalk();
+
+// 2 task. Compare Scopes of the var and let Keywords
+
+function checkScope() {
+	let i = 'function scope';
+	if (true) {
+		i = 'block scope';
+		console.log('Block scope i is: ', i);
+	}
+	i = 'function scope';
+	console.log('Function scope i is: ', i);
+	return i;
+}
