c9ea9a08c052882d19e0891346ab0b9d99eafc0e
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-14 18:13:31 +0300

    solved 5 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -50,9 +50,23 @@ function editInPlace() {
 
 editInPlace();
 
+// 5 task. Prevent Object Mutation
 
+function freezeObj() {
+	const MATH_CONSTANTS = {
+		PI: 3.14
+	};
 
+	Object.freeze(MATH_CONSTANTS);
 
+	try {
+		MATH_CONSTANTS.PI = 99;
+	} catch (ex) {
+		console.log(ex);
+	}
+	return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
 
 
 
