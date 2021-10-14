10e182e182e770610f473d7852adff0c4fc35d99
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-14 15:26:14 +0300

    solved 108 task

--- Basic JavaScript/javascript/freeCodeCamp.js
+++ Basic JavaScript/javascript/freeCodeCamp.js
@@ -1199,9 +1199,15 @@ function convertToInteger(str) {
 
 convertToInteger("10011");
 
+// 108 task. Use the Conditional (Ternary) Operator
 
+function checkEqual(a, b) {
 
+	return a == b ? 'Equal' : 'Not Equal';
 
+}
+
+checkEqual(1, 2);
 
 
 
