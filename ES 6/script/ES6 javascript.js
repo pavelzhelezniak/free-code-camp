42b1f3b0470c6b7b4f977a826a520742c4232558
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-15 15:20:52 +0300

    solved 15 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -136,3 +136,15 @@ const { today: { low: lowToday, high: highToday } } = LOCAL_FORECAST;
 let a = 8, b = 6;
 
 [a, b] = [b, a];
+
+// 15 task. Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeFirstTwo(list) {
+	// Only change code below this line
+	// const arr = list; Change this line
+	const [a, b, ...arr] = list;
+	// Only change code above this line
+	return arr;
+}
+const arr = removeFirstTwo(source);
