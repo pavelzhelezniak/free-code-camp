a913e38f94709950e8ff923e4474db9aba625150
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-16 09:52:44 +0300

    solved 21 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -217,3 +217,50 @@ class Vegetable {
 
 const carrot = new Vegetable('carrot');
 console.log(carrot.name);
+
+// 21 task. Use getters and setters to Control Access to an Object
+
+class Thermostat {
+	constructor(temperature) {
+		this._temperature = temperature;
+	}
+	get temperature() {
+		return 5 / 9 * (this._temperature - 32);
+	}
+	set temperature(updateTemperature) {
+		this._temperature = updateTemperature * 9.0 / 5 + 32;
+	}
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
