0c3974fc3a74b419d3443fe185efd0427556d8c6
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-16 10:29:07 +0300

    solved 29 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -291,3 +291,44 @@ const makeServerRequest = new Promise((resolve, reject) => {
 
 });
 
+// 29 task. Complete a Promise with resolve and reject
+
+const makeServerRequest = new Promise((resolve, reject) => {
+	// responseFromServer represents a response from a server
+	let responseFromServer;
+
+	if (responseFromServer) {
+		resolve("We got the data") // Change this line
+	} else {
+		reject("Data not received") // Change this line
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
