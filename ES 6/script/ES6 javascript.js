5f3266035f7d81b33e261f6ee513b314a7dace40
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-16 10:38:24 +0300

    solved 31 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -321,27 +321,19 @@ makeServerRequest.then(result => {
 	console.log(result);
 });
 
+// 31 task. Handle a Rejected Promise with catch
 
+const makeServerRequest = new Promise((resolve, reject) => {
+	// responseFromServer is set to false to represent an unsuccessful response from a server
+	let responseFromServer = false;
 
+	if (responseFromServer) {
+		resolve("We got the data");
+	} else {
+		reject("Data not received");
+	}
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+makeServerRequest.catch(error => {
+	console.log(error);
+});
