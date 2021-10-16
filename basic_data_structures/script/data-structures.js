237f77d1529d0ad72fdc380b91ad12d4eeb934db
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-16 14:38:51 +0300

    solved 16 task by data structures

--- basic_data_structures/script/data-structures.js
+++ basic_data_structures/script/data-structures.js
@@ -168,3 +168,20 @@ function checkInventory(scannedItem) {
 }
 
 console.log(checkInventory("apples"));
+
+// 16 task. Use the delete Keyword to Remove Object Properties
+
+let foods = {
+	apples: 25,
+	oranges: 32,
+	plums: 28,
+	bananas: 13,
+	grapes: 35,
+	strawberries: 27
+};
+
+delete foods.oranges;
+delete foods.plums;
+delete foods.strawberries;
+
+console.log(foods);
