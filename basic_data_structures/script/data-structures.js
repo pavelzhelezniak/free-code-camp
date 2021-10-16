53f1fc6db854898fa2af869477822f3350687006
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-16 12:18:15 +0300

    solved 3 task by data structures

--- basic_data_structures/script/data-structures.js
+++ basic_data_structures/script/data-structures.js
@@ -7,3 +7,15 @@ let yourArray = ['string', 19, false, [1, 9], { a: 1, b: 5 }];
 let myArray = ["a", "b", "c", "d"];
 myArray[1] = 'index = 1'
 console.log(myArray);
+
+// 3 task. Add Items to an Array with push() and unshift()
+
+function mixedNumbers(arr) {
+	// Only change code below this line
+	arr.unshift('I', 2, 'three')
+	arr.push(7, 'VIII', 9)
+	// Only change code above this line
+	return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
