becc7f96859cbef4073b6065c991df5b933c9137
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-16 09:19:42 +0300

    solved 19 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -194,8 +194,17 @@ const createPerson = (name, age, gender) => {
 	// Only change code above this line
 };
 
+// 19 task. Write Concise Declarative Functions with ES6
 
-
-
+// Only change code below this line
+const bicycle = {
+	gear: 2,
+	setGear(newGear) {
+		this.gear = newGear;
+	}
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
 
 
