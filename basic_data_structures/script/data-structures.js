6a1f9fa20dbe51119e98322318f5283ae3c76a2b
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-16 14:36:06 +0300

    solved 15 task by data structures

--- basic_data_structures/script/data-structures.js
+++ basic_data_structures/script/data-structures.js
@@ -149,3 +149,22 @@ let userActivity = {
 userActivity['data']['online'] = 45
 
 console.log(userActivity);
+
+// 15 task. Access Property Names with Bracket Notation
+
+let foods = {
+	apples: 25,
+	oranges: 32,
+	plums: 28,
+	bananas: 13,
+	grapes: 35,
+	strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+
+	return foods[scannedItem];
+
+}
+
+console.log(checkInventory("apples"));
