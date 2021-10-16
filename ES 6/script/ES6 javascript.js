5d7b48d99f85812e61e6bb79c7a667935df4cb0f
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-16 10:01:45 +0300

    solved 23 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -246,9 +246,17 @@ temp = thermos.temperature; // 26 in Celsius
 	</body>
 </html>
 
+// 23 task. Use export to Share a Code Block
 
+const uppercaseString = (string) => {
+	return string.toUpperCase();
+}
 
+const lowercaseString = (string) => {
+	return string.toLowerCase()
+}
 
+export { uppercaseString, lowercaseString };
 
 
 
