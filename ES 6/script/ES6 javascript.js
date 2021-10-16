4684eda71101c726562e767a842e758101c3da07
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-16 09:13:58 +0300

    solved 17 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -163,12 +163,28 @@ const stats = {
 // Only change code below this line
 // const half = (stats) => (stats.max + stats.min) / 2.0;
 const half = ({ max, min }) => (max + min) / 2.0;
- // Only change code above this line
+// Only change code above this line
+
+// 17 task. Create Strings using Template Literals
 
 
+const result = {
+	success: ["max-length", "no-amd", "prefer-arrow-functions"],
+	failure: ["no-var", "var-on-top", "linebreak"],
+	skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+	// Only change code below this line
+	const failureItems = [];
+	for (let i = 0; i < arr.length; i++) {
+		failureItems.push(`<li class="text-warning">${arr[i]}</li>`)
+	}
+	// Only change code above this line
 
+	return failureItems;
+}
 
-
+const failuresList = makeList(result.failure);
 
 
 
