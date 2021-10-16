302273ccda80570e4905affc1d69816babe09650
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-16 14:43:29 +0300

    solved 17 task by data structures

--- basic_data_structures/script/data-structures.js
+++ basic_data_structures/script/data-structures.js
@@ -185,3 +185,34 @@ delete foods.plums;
 delete foods.strawberries;
 
 console.log(foods);
+
+// 17 task. Check if an Object has a Property
+
+let users = {
+	Alan: {
+		age: 27,
+		online: true
+	},
+	Jeff: {
+		age: 32,
+		online: true
+	},
+	Sarah: {
+		age: 48,
+		online: true
+	},
+	Ryan: {
+		age: 19,
+		online: true
+	}
+};
+
+function isEveryoneHere(userObj) {
+
+	if (userObj.hasOwnProperty('Alan') && userObj.hasOwnProperty('Jeff') && userObj.hasOwnProperty('Sarah') && userObj.hasOwnProperty('Ryan')) {
+		return true;
+	} else return false;
+
+}
+
+console.log(isEveryoneHere(users));
