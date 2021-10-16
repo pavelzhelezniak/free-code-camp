1d184f6a65c0894d181ff5106ff95557a91758de
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-16 12:37:32 +0300

    solved 8 task by data structures

--- basic_data_structures/script/data-structures.js
+++ basic_data_structures/script/data-structures.js
@@ -55,3 +55,18 @@ function forecast(arr) {
 }
 
 console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+// 8 task. Copy an Array with the Spread Operator
+
+function copyMachine(arr, num) {
+	let newArr = [];
+	while (num >= 1) {
+		// Only change code below this line
+		newArr.push([...arr])
+		// Only change code above this line
+		num--;
+	}
+	return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
