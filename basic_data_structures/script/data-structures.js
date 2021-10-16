bf588a538e54bb43d716137d8c34e913efa43505
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-16 12:15:30 +0300

    solved 2 task by data structures

--- basic_data_structures/script/data-structures.js
+++ basic_data_structures/script/data-structures.js
@@ -1,3 +1,9 @@
 // 1 task. Use an Array to Store a Collection of Data
 
 let yourArray = ['string', 19, false, [1, 9], { a: 1, b: 5 }];
+
+// 2 task. Access an Array's Contents Using Bracket Notation
+
+let myArray = ["a", "b", "c", "d"];
+myArray[1] = 'index = 1'
+console.log(myArray);
