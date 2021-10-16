ca3e66619bb4fa884b745507d8a211bfc5466e23
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-16 12:34:19 +0300

    solved 7 task by data structures

--- basic_data_structures/script/data-structures.js
+++ basic_data_structures/script/data-structures.js
@@ -46,3 +46,12 @@ function htmlColorNames(arr) {
 }
 
 console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+// 7 task. Copy Array Items Using slice()
+
+function forecast(arr) {
+	const newArray = arr.slice(2, 4)
+	return newArray;
+}
+
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
