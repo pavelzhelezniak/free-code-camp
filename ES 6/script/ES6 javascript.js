d220b6a98b91c18c2038ea9792e1580031cf93a7
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-16 10:08:17 +0300

    solved 25 task by ES6

--- ES 6/script/ES6 javascript.js
+++ ES 6/script/ES6 javascript.js
@@ -265,9 +265,13 @@ import { uppercaseString, lowercaseString } from './string_functions.js';
 uppercaseString("hello");
 lowercaseString("WORLD!");
 
+// 25 task. Use * to Import Everything from a File
 
+import * as stringFunctions from "./string_functions.js"
+// Only change code above this line
 
-
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
 
 
 
