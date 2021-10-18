5d881814c42757f63148b18f1663196cef556a28
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-18 12:21:10 +0300

    solved 7 task by reg-exp

--- regular_expressions/script/reg-exp.js
+++ regular_expressions/script/reg-exp.js
@@ -33,3 +33,9 @@ let result = extractStr.match(codingRegex);
 let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /twinkle/gi; // Change this line
 let result = twinkleStar.match(starRegex); // Change this line
+
+// 7 task. Match Anything with Wildcard Period
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
