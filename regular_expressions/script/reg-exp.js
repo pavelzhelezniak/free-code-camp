eb5233ead7670d1cbfe5eec2509b6e5d1ebff9be
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-18 12:11:54 +0300

    solved 4 task by reg-exp

--- regular_expressions/script/reg-exp.js
+++ regular_expressions/script/reg-exp.js
@@ -10,8 +10,14 @@ let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/;
 let result = waldoRegex.test(waldoIsHiding);
 
-// 2 task. Match a Literal String with Different Possibilities
+// 3 task. Match a Literal String with Different Possibilities
 
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; // Change this line
 let result = petRegex.test(petString);
+
+// 4 task. Ignore Case While Matching
+
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; // Change this line
+let result = fccRegex.test(myString);
