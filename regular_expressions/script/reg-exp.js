486aea592c80807f3e7939546351fa236772bb13
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-18 12:32:56 +0300

    solved 9 task by reg-exp

--- regular_expressions/script/reg-exp.js
+++ regular_expressions/script/reg-exp.js
@@ -45,3 +45,9 @@ let result = unRegex.test(exampleStr);
 let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 let vowelRegex = /[aeiou]/gi; // Change this line
 let result = quoteSample.match(vowelRegex); // Change this line
+
+// 9 task. Match Letters of the Alphabet
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
