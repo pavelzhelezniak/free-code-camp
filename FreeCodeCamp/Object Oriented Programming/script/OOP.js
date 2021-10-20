96cd84b9666dc9da15a4c2f31ded3b92dd587893
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-20 14:32:57 +0300

    solved 20 task by OOP

--- FreeCodeCamp/Object Oriented Programming/script/OOP.js
+++ FreeCodeCamp/Object Oriented Programming/script/OOP.js
@@ -239,3 +239,19 @@ Animal.prototype = {
 function Dog() { }
 Dog.prototype = Object.create(Animal.prototype);
 let beagle = new Dog();
+
+// 20 Reset an Inherited Constructor Property
+
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+
+let duck = new Bird();
+let beagle = new Dog();
