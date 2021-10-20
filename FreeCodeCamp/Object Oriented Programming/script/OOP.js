42b23d2cdefee918512af19d89354f40a19d7b9e
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-20 13:03:51 +0300

    solved 11 task by OOP

--- FreeCodeCamp/Object Oriented Programming/script/OOP.js
+++ FreeCodeCamp/Object Oriented Programming/script/OOP.js
@@ -94,3 +94,24 @@ function Dog(name) {
 }
 Dog.prototype.numLegs = 4;
 let beagle = new Dog("Snoopy");
+
+// 11 task. Iterate Over All Properties
+
+function Dog(name) {
+	this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+for (let property in beagle) {
+	if (beagle.hasOwnProperty(property)) {
+		ownProps.push(property);
+	} else {
+		prototypeProps.push(property);
+	}
+}
