30b79b74c5d464f16fd086e747dc2fb148ab89c3
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-20 14:30:23 +0300

    solved 19 task by OOP

--- FreeCodeCamp/Object Oriented Programming/script/OOP.js
+++ FreeCodeCamp/Object Oriented Programming/script/OOP.js
@@ -224,3 +224,18 @@ Animal.prototype = {
 
 let duck = Object.create(Animal.prototype);
 let beagle = Object.create(Animal.prototype);
+
+// 19 task. Set the Child's Prototype to an Instance of the Parent
+
+function Animal() { }
+
+Animal.prototype = {
+	constructor: Animal,
+	eat: function () {
+		console.log("nom nom nom");
+	}
+};
+
+function Dog() { }
+Dog.prototype = Object.create(Animal.prototype);
+let beagle = new Dog();
