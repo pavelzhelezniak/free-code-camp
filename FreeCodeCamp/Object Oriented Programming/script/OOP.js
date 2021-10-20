d007efe9fb842a657856f79936944b0880ba6b1d
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-20 13:00:43 +0300

    solved 10 task by OOP

--- FreeCodeCamp/Object Oriented Programming/script/OOP.js
+++ FreeCodeCamp/Object Oriented Programming/script/OOP.js
@@ -86,3 +86,11 @@ for (let property in canary) {
 		ownProps.push(property);
 	}
 }
+
+// 10 task. Use Prototype Properties to Reduce Duplicate Code
+
+function Dog(name) {
+	this.name = name;
+}
+Dog.prototype.numLegs = 4;
+let beagle = new Dog("Snoopy");
