5282ae6bc9450a257aaf3400a70757d652ded421
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-20 12:01:54 +0300

    solved 2 task by OOP

--- FreeCodeCamp/Object Oriented Programming/script/OOP.js
+++ FreeCodeCamp/Object Oriented Programming/script/OOP.js
@@ -4,3 +4,13 @@ let dog = {
 	name: 'Alfa',
 	numLegs: 4
 };
+
+// 2 task. Use Dot Notation to Access the Properties of an Object
+
+let dog = {
+	name: "Spot",
+	numLegs: 4
+};
+
+console.log(dog.name);
+console.log(dog.numLegs);
