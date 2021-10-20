428fa5d05ed665262cbdbce47b6a4f7c6bfadc5a
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-20 12:23:28 +0300

    solved 6 task by OOP

--- FreeCodeCamp/Object Oriented Programming/script/OOP.js
+++ FreeCodeCamp/Object Oriented Programming/script/OOP.js
@@ -44,3 +44,13 @@ function Dog() {
 	this.color = "black";
 	this.numLegs = 4;
 }
+
+// 6 task. Use a Constructor to Create Objects
+
+function Dog() {
+	this.name = "Rupert";
+	this.color = "brown";
+	this.numLegs = 4;
+}
+
+let hound = new Dog();
