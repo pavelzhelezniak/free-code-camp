f40632d959becac7248d80073e40b112e643865c
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-20 12:17:01 +0300

    solved 4 task by OOP

--- FreeCodeCamp/Object Oriented Programming/script/OOP.js
+++ FreeCodeCamp/Object Oriented Programming/script/OOP.js
@@ -26,3 +26,13 @@ let dog = {
 };
 
 dog.sayLegs();
+
+// 4 task. Make Code More Reusable with the this Keyword
+
+let dog = {
+	name: "Spot",
+	numLegs: 4,
+	sayLegs: function () { return "This dog has " + this.numLegs + " legs."; }
+};
+
+dog.sayLegs();
