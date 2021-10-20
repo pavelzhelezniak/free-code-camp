2be09b5413badd9d9920b478b93113761cae9e01
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-20 14:39:07 +0300

    solved 21 task by OOP

--- FreeCodeCamp/Object Oriented Programming/script/OOP.js
+++ FreeCodeCamp/Object Oriented Programming/script/OOP.js
@@ -255,3 +255,15 @@ Dog.prototype.constructor = Dog;
 
 let duck = new Bird();
 let beagle = new Dog();
+
+// 21 Add Methods After Inheritance
+
+function Animal() { }
+Animal.prototype.eat = function () { console.log("nom nom nom"); };
+
+function Dog() { }
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function () { console.log("Woof!"); };
+
+let beagle = new Dog();
