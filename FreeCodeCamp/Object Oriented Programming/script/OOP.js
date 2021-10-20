29c2b07cfa45c1e4b4532eb9be562c26ba6008c5
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-20 13:57:14 +0300

    solved 17 task by OOP

--- FreeCodeCamp/Object Oriented Programming/script/OOP.js
+++ FreeCodeCamp/Object Oriented Programming/script/OOP.js
@@ -183,3 +183,30 @@ let beagle = new Dog("Snoopy");
 
 Dog.prototype.isPrototypeOf(beagle);  // yields true
 Object.prototype.isPrototypeOf(Dog.prototype); // true
+
+// 17 task. Use Inheritance So You Don't Repeat Yourself
+
+function Cat(name) {
+	this.name = name;
+}
+
+Cat.prototype = {
+	constructor: Cat,
+};
+
+function Bear(name) {
+	this.name = name;
+}
+
+Bear.prototype = {
+	constructor: Bear,
+};
+
+function Animal() { }
+
+Animal.prototype = {
+	constructor: Animal,
+	eat: function () {
+		console.log("nom nom nom");
+	}
+};
