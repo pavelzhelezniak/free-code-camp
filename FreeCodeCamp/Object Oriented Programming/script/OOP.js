a30fe20058a73075ac244f2e2f652743d8f3436b
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-20 14:50:50 +0300

    solved 22 task by OOP

--- FreeCodeCamp/Object Oriented Programming/script/OOP.js
+++ FreeCodeCamp/Object Oriented Programming/script/OOP.js
@@ -267,3 +267,17 @@ Dog.prototype.constructor = Dog;
 Dog.prototype.bark = function () { console.log("Woof!"); };
 
 let beagle = new Dog();
+
+// 22 Override Inherited Methods
+
+function Bird() { }
+
+Bird.prototype.fly = function () { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+Penguin.prototype.fly = function () { return "Alas, this is a flightless bird."; };
+
+let penguin = new Penguin();
+console.log(penguin.fly());
