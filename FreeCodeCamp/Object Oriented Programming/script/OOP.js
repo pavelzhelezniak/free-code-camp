7be4e69af4f07bde25495fb67fe2c44aa1e08060
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-20 12:06:20 +0300

    solved 3 task by OOP

--- FreeCodeCamp/Object Oriented Programming/script/OOP.js
+++ FreeCodeCamp/Object Oriented Programming/script/OOP.js
@@ -14,3 +14,15 @@ let dog = {
 
 console.log(dog.name);
 console.log(dog.numLegs);
+
+// 3 task. Create a Method on an Object
+
+let dog = {
+	name: "Spot",
+	numLegs: 4,
+	sayLegs: function () {
+		return 'This dog has ' + dog.numLegs + ' legs.'
+	}
+};
+
+dog.sayLegs();
