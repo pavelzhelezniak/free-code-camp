32a5889d56b38b025ef0b84e98bfd36c60732889
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-21 18:11:38 +0300

    solved 1 task by functional programming

---
+++ FreeCodeCamp/Functional Programming/script/functional-programming.js
@@ -0,0 +1,15 @@
+// 1 task. Learn About Functional Programming
+
+const prepareTea = () => 'greenTea';
+
+const getTea = (numOfCups) => {
+	const teaCups = [];
+
+	for (let cups = 1; cups <= numOfCups; cups += 1) {
+		const teaCup = prepareTea();
+		teaCups.push(teaCup);
+	}
+	return teaCups;
+};
+
+const tea4TeamFCC = getTea(40);
