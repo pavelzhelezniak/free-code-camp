c57dad355f2f103e3c4cc7a9a0f0cdd3f34421bf
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-21 18:20:02 +0300

    solved 2 task by functional programming

--- FreeCodeCamp/Functional Programming/script/functional-programming.js
+++ FreeCodeCamp/Functional Programming/script/functional-programming.js
@@ -13,3 +13,22 @@ const getTea = (numOfCups) => {
 };
 
 const tea4TeamFCC = getTea(40);
+
+// 2 task. Understand Functional Programming Terminology
+
+const prepareGreenTea = () => 'greenTea';
+const prepareBlackTea = () => 'blackTea';
+
+const getTea = (prepareTea, numOfCups) => {
+	const teaCups = [];
+
+	for (let cups = 1; cups <= numOfCups; cups += 1) {
+		const teaCup = prepareTea();
+		teaCups.push(teaCup);
+	}
+	return teaCups;
+};
+
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+console.log(tea4GreenTeamFCC, tea4BlackTeamFCC);
