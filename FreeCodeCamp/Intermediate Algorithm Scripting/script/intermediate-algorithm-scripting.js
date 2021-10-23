453a39e237a8cb22fd491883780d59d23460c047
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-23 15:11:30 +0300

    solved 2 task by intermediate-algorithm-scripting

--- FreeCodeCamp/Intermediate Algorithm Scripting/script/intermediate-algorithm-scripting.js
+++ FreeCodeCamp/Intermediate Algorithm Scripting/script/intermediate-algorithm-scripting.js
@@ -18,3 +18,25 @@ function sumAll(arr) {
 }
 
 console.log(sumAll([4, 1]));
+
+// 2 task. Diff Two Arrays
+
+function diffArray(arr1, arr2) {
+	var newArr = [];
+
+	for (let i = 0; i < arr1.length; i++) {
+		if (!arr2.includes(arr1[i])) {
+			newArr.push(arr1[i]);
+		}
+	}
+
+	for (let i = 0; i < arr2.length; i++) {
+		if (!arr1.includes(arr2[i])) {
+			newArr.push(arr2[i]);
+		}
+	}
+
+	return newArr;
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
