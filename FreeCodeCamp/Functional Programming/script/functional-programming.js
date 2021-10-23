198591599f9e67514d570d6682f6a3b7963a8ba9
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-23 13:45:01 +0300

    solved 23 task by functional programming

--- FreeCodeCamp/Functional Programming/script/functional-programming.js
+++ FreeCodeCamp/Functional Programming/script/functional-programming.js
@@ -660,3 +660,13 @@ function checkPositive(arr) {
 	// Only change code above this line
 }
 console.log(checkPositive([1, 2, 3, -4, 5]));
+
+// 23 task. Use the some Method to Check that Any Elements in an Array Meet a Criteria
+
+function checkPositive(arr) {
+	// Only change code below this line
+	return arr.some(item => item > 0);
+
+	// Only change code above this line
+}
+checkPositive([1, 2, 3, -4, 5]);
