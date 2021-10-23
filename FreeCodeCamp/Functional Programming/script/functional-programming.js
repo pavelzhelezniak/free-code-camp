a9f6722cafd8367ad28e13e02a325478856c9c35
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-23 12:21:55 +0300

    solved 18 task by functional programming

--- FreeCodeCamp/Functional Programming/script/functional-programming.js
+++ FreeCodeCamp/Functional Programming/script/functional-programming.js
@@ -608,3 +608,14 @@ function alphabeticalOrder(arr) {
 	// Only change code above this line
 }
 console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"]));
+
+// 18 task. Return a Sorted Array Without Changing the Original Array
+
+var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+	// Only change code below this line
+	var newArray = [].concat(arr);
+	return newArray.sort(function (a, b) { return a - b });
+	// Only change code above this line
+}
+console.log(nonMutatingSort(globalArray));
