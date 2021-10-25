a317ba4c083a58aa483995e79e2ff9e7ccae36ce
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-25 17:32:45 +0300

    solved 18 task by intermediate-algorithm-scripting

--- FreeCodeCamp/Intermediate Algorithm Scripting/script/intermediate-algorithm-scripting.js
+++ FreeCodeCamp/Intermediate Algorithm Scripting/script/intermediate-algorithm-scripting.js
@@ -158,3 +158,16 @@ function binaryAgent(str) {
 }
 
 console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
+
+// 18 task. Everything Be True
+
+function truthCheck(collection, pre) {
+
+	return collection.every((item) => {
+		return item.hasOwnProperty(pre) && Boolean(item[pre]);
+
+	});
+}
+
+console.log(truthCheck([{ "user": "Tinky-Winky", "sex": "male" }, { "user": "Dipsy", "sex": "male" }, { "user": "Laa-Laa", "sex": "female" }, { "user": "Po", "sex": "female" }], "sex"));
+console.log(truthCheck([{ "user": "Tinky-Winky", "sex": "male", "age": 0 }, { "user": "Dipsy", "sex": "male", "age": 3 }, { "user": "Laa-Laa", "sex": "female", "age": 5 }, { "user": "Po", "sex": "female", "age": 4 }], "age"));
