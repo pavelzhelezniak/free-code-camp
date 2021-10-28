e4bdd6cbdbc7e84bd03b401c368c84ec8dcd58e2
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-28 14:07:40 +0300

    solved 2 task by debugging

--- FreeCodeCamp/Debugging/script/debugging.js
+++ FreeCodeCamp/Debugging/script/debugging.js
@@ -5,3 +5,9 @@ let b = 1;
 a++;
 let sumAB = a + b;
 console.log(a);
+
+// 2 task. Understanding the Differences between the freeCodeCamp and Browser Console
+
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear();
