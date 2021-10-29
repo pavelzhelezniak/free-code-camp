e78e6fc55d383ca0d2ed35ff1f80bfb48d8c5073
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-29 11:17:41 +0300

    solved 5 task by debugging

--- FreeCodeCamp/Debugging/script/debugging.js
+++ FreeCodeCamp/Debugging/script/debugging.js
@@ -26,3 +26,9 @@ let receivables = 10;
 let payables = 8;
 let netWorkingCapital = receivables - payables;
 console.log(`Net working capital is: ${netWorkingCapital}`);
+
+// 5 task. Catch Unclosed Parentheses, Brackets, Braces and Quotes
+
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) => previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
