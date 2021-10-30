e58fe4778294b5b87f754dad555199f638053567
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-30 15:06:51 +0300

    solved 8 task by debugging

--- FreeCodeCamp/Debugging/script/debugging.js
+++ FreeCodeCamp/Debugging/script/debugging.js
@@ -51,3 +51,14 @@ if (x === y) {
 }
 
 console.log(result);
+
+// 8 task. Catch Missing Open and Closing Parenthesis After a Function Call
+
+function getNine() {
+	let x = 6;
+	let y = 3;
+	return x + y;
+}
+
+let result = getNine();
+console.log(result);
