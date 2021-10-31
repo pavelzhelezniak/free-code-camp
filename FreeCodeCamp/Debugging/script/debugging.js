cf80b7269d5f5c8dd29f3759d31dbce736b050f3
pavelzhelezniak <zhele3o1992@gmail.com>
2021-10-31 17:30:15 +0300

    solved 11 task by debugging

--- FreeCodeCamp/Debugging/script/debugging.js
+++ FreeCodeCamp/Debugging/script/debugging.js
@@ -87,3 +87,25 @@ function countToFive() {
 }
 
 countToFive();
+
+// 11 task. Use Caution When Reinitializing Variables Inside a Loop
+
+function zeroArray(m, n) {
+	// Creates a 2-D array with m rows and n columns of zeroes
+	let newArray = [];
+	let row = [];
+	for (let i = 0; i < m; i++) {
+		// Adds the m-th row into newArray
+
+		for (let j = 0; j < n; j++) {
+			// Pushes n zeroes into the current row to create the columns
+			row[j] = 0;
+		}
+		// Pushes the current row, which now has n zeroes in it, to the array
+		newArray.push(row);
+	}
+	return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
