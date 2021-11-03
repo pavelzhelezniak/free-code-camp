ba9169d65e66d028b750b0050a4cf8edd55973e6
pavelzhelezniak <zhele3o1992@gmail.com>
2021-11-03 14:45:27 +0300

    solved 8 task by basic-algorithm-scripting

--- FreeCodeCamp/Basic Algorithm Scripting/script/basic-algorithm-scripting.js
+++ FreeCodeCamp/Basic Algorithm Scripting/script/basic-algorithm-scripting.js
@@ -79,3 +79,15 @@ function repeatStringNumTimes(str, num) {
 }
 
 repeatStringNumTimes("abc", 3);
+
+// 8 task. Truncate a String
+
+function truncateString(str, num) {
+	if (str.length > num) {
+		return str.slice(0, num) + '...';
+	} else {
+		return str;
+	}
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
