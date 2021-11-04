af215a3a736bf68947c22e4fc360ec78100cc9b9
pavelzhelezniak <zhele3o1992@gmail.com>
2021-11-04 16:36:22 +0300

    solved 9 task by basic-algorithm-scripting

--- FreeCodeCamp/Basic Algorithm Scripting/script/basic-algorithm-scripting.js
+++ FreeCodeCamp/Basic Algorithm Scripting/script/basic-algorithm-scripting.js
@@ -91,3 +91,20 @@ function truncateString(str, num) {
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+// 9 task. Finders Keepers
+
+function findElement(arr, func) {
+	let num = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (func(arr[i])) {
+			num = arr[i];
+			break;
+		} else {
+			num = undefined;
+		}
+	}
+	return num;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
