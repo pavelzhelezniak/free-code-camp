87addd8d7b422a5c01dfd664d3caf73460ebcb74
pavelzhelezniak <zhele3o1992@gmail.com>
2021-11-05 17:02:30 +0300

    solved 11 task by basic-algorithm-scripting

--- FreeCodeCamp/Basic Algorithm Scripting/script/basic-algorithm-scripting.js
+++ FreeCodeCamp/Basic Algorithm Scripting/script/basic-algorithm-scripting.js
@@ -116,3 +116,19 @@ function booWho(bool) {
 }
 
 booWho(null);
+
+// 11 task. Title Case a Sentence
+
+function titleCase(str) {
+	str = str.split(' ');
+
+	for (let i = 0; i < str.length; i++) {
+		str[i] = str[i].toLowerCase().split('');
+		str[i][0] = str[i][0].toUpperCase();
+		str[i] = str[i].join('');
+	}
+
+	return str.join(' ');
+}
+
+titleCase("I'm a little tea pot");
